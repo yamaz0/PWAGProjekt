88c8d1b6f91d4938b6b589b09ca27f4f35aecb1b
yamaz0 <>
2020-10-15 23:17:47 +0200

    glew, glfw, cube vertex buffer

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,43 +1,164 @@
-//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-//to tylko przyklad z losowej strony//
-//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#include <iostream>
+#include <fstream>
 
-#include <GL/freeglut.h>   // freeglut.h might be a better alternative, if available.
+#define GLUT_DISABLE_ATEXIT_HACK
+#include "GL/glew.h"
+#include "GL/freeglut.h"
 
-void display() {  // Display function will draw the image.
+using namespace std;
+// ----------------------------------------------------------
+// Function Prototypes
+// ----------------------------------------------------------
+void display();
+void specialKeys();
+void setup();
 
-	glClearColor(0, 0, 0, 1);  // (In fact, this is the default.)
-	glClear(GL_COLOR_BUFFER_BIT);
+// ----------------------------------------------------------
+// Global Variables
+// ----------------------------------------------------------
+double rotate_y = 0;
+double rotate_x = 0;
 
-	glBegin(GL_TRIANGLES);
-	glColor3f(1, 0, 0); // red
-	glVertex2f(-0.8, -0.8);
-	glColor3f(0, 1, 0); // green
-	glVertex2f(0.8, -0.8);
-	glColor3f(0, 0, 1); // blue
-	glVertex2f(0, 0.9);
-	glEnd();
+void setup()
+{
+	float const s = 0.5;
+	float cube[108] = {
 
-	glutSwapBuffers(); // Required to copy color buffer onto the screen.
+		-s, -s,  s,		 s, -s,  s,		 s,  s,  s,
+		-s, -s,  s,		 s,  s,  s,		-s,  s,  s,
+
+		-s,  -s,  s,	-s, -s,  -s,	 s,  -s,  s,
+		-s,  -s, -s,	 s, -s,  -s,	 s,  -s,  s,
+
+		s, -s,  s,		 s, -s, -s,		 s,  s, -s,
+		s, -s,  s,		 s,  s, -s,		 s,  s,  s,
+
+		s,  s, -s,		 s, -s, -s,		-s, -s, -s,
+		s,  s, -s,		-s, -s, -s,		-s,  s, -s,
+
+		-s,  s, -s,		-s, -s, -s,		-s, -s,  s,
+		-s,  s, -s,		-s, -s,  s,		-s,  s,  s,
+
+		-s,  s, -s,		-s,  s,  s,		 s,  s,  s,
+		-s,  s, -s,		 s,  s,  s,		 s,  s, -s
+	};	
+	float cubeColor[144] = {
+
+		1,1,1,1,	1,1,1,1,	1,1,1,1,
+		1,1,1,1,	1,1,1,1,	1,1,1,1,
+
+		1,0,0,1,	1,0,0,1,	1,0,0,1,
+		1,0,0,1,	1,0,0,1,	1,0,0,1,
+
+		0,1,0,1,	0,1,0,1,	0,1,0,1,
+		0,1,0,1,	0,1,0,1,	0,1,0,1,
+
+		0,0,1,1,	0,0,1,1,	0,0,1,1,
+		0,0,1,1,	0,0,1,1,	0,0,1,1,
+
+		1,1,0,1,	1,1,0,1,	1,1,0,1,
+		1,1,0,1,	1,1,0,1,	1,1,0,1,
+
+		1,0,1,1,	1,0,1,1,	1,0,1,1,
+		1,0,1,1,	1,0,1,1,	1,0,1,1
+	};
+
+	unsigned int buffer;
+
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, 108 * sizeof(float), cube, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,sizeof(float)*3,0);
+}
+
+// ----------------------------------------------------------
+// display() Callback function
+// ----------------------------------------------------------
+void display()
+{
+
+	//  Clear screen and Z-buffer
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// Reset transformations
+	glLoadIdentity();
+
+	// Other Transformations
+	// glTranslatef( 0.1, 0.0, 0.0 );      // Not included
+	// glRotatef( 180, 0.0, 1.0, 0.0 );    // Not included
+
+	// Rotate when user changes rotate_x and rotate_y
+	glRotatef(rotate_x, 1.0, 0.0, 0.0);
+	glRotatef(rotate_y, 0.0, 1.0, 0.0);
+
+	for (int i = 0; i < 12; i++)
+	{
+		glDrawArrays(GL_TRIANGLES, i*3, 3);
+	}
+
+	glFlush();
+	glutSwapBuffers();
+}
+
+// ----------------------------------------------------------
+// specialKeys() Callback Function
+// ----------------------------------------------------------
+void specialKeys(int key, int x, int y)
+{
+	//  Right arrow - increase rotation by 5 degree
+	if (key == GLUT_KEY_RIGHT)
+		rotate_y += 5;
+
+	//  Left arrow - decrease rotation by 5 degree
+	else if (key == GLUT_KEY_LEFT)
+		rotate_y -= 5;
+
+	else if (key == GLUT_KEY_UP)
+		rotate_x += 5;
+
+	else if (key == GLUT_KEY_DOWN)
+		rotate_x -= 5;
+
+	//  Request display update
+	glutPostRedisplay();
 
 }
 
+// ----------------------------------------------------------
+// main() function
+// ----------------------------------------------------------
+int main(int argc, char* argv[])
+{
 
-int main(int argc, char** argv) {  // Initialize GLUT and 
-
+	//  Initialize GLUT and process user parameters
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE);    // Use single color buffer and no depth buffer.
-	glutInitWindowSize(500, 500);         // Size of display area, in pixels.
-	glutInitWindowPosition(100, 100);     // Location of window in screen coordinates.
-	glutCreateWindow("GL RGB Triangle"); // Parameter is window title.
-	glutDisplayFunc(display);            // Called when the window needs to be redrawn.
 
-	glutMainLoop(); // Run the event loop!  This function does not return.
-					// Program ends when user closes the window.
+	//  Request double buffered true color window with Z-buffer
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(100, 100);
+	// Create window
+	glutCreateWindow("Awesome Cube");
+
+	GLenum err = glewInit();
+	if (GLEW_OK != err)
+	{
+		/* Problem: glewInit failed, something is seriously wrong. */
+		fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
+	}
+
+	//  Enable Z-buffer depth test
+	glEnable(GL_DEPTH_TEST);
+	setup();
+	// Callback functions
+	glutDisplayFunc(display);
+	glutSpecialFunc(specialKeys);
+
+	//  Pass control to GLUT for events
+	glutMainLoop();
+
+	//  Return to OS
 	return 0;
 
 }
-
-//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-//to tylko przyklad z losowej strony//
-//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
